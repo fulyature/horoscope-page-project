5ef831eb1ecfac89636d9335f5f6fff6bd5f7eac
fulya_ture <fulyature@gmail.com>
2023-07-18 23:29:20 +0300

    Update styles

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -6,9 +6,7 @@ const Card = ({ title, date, image, desc }) => {
         {/* <h1 className="title">{props.title}</h1> // destraction yapmadan bu şekılde de yazılabılır */}
         <h1 className="title">{title}</h1>
       </div>
-      <div className="date">
-        <h2 className="date">{date}</h2>
-      </div>
+      <h2 className="date">{date}</h2>
       <img src={image} alt="" />
       <div className="card-over">
         <p>{desc}</p>
